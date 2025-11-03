3bec506d1e197713813c379714ab026cf399c126
smshine <smshine@qq.com>
2025-11-03 09:17:21 +0800

    add zh_cn strings

---
+++ lang/zh_cn/questionnaire.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'questionnaire', language 'zh_cn' (简体中文)
+ *
+ * @package    mod_questionnaire
+ * @copyright  2025
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// 个性化文件相关字符串
+$string['personalfileenabled'] = '启用个性化文件';
+$string['personalfileenabled_help'] = '启用后,每个学生在填写问卷时只能看到以自己学号命名的个性化文件。适用于个人照片投票或其他需要隐私保护的场景。';
+$string['personalfile_upload'] = '上传个性化文件';
+$string['personalfile_uploadhelp'] = '请上传以学号命名的文件(例如:20231001.jpg, 20231002.png)。每个学生在填写问卷时只能看到自己的文件。';
+$string['personalfile_imported'] = '成功导入 {$a} 个文件。';
+$string['personalfile_usernotfound'] = '找不到学号为 "{$a}" 的学生。';
+$string['personalfile_usernotenrolled'] = '学号为 "{$a}" 的学生未注册本课程。';
+$string['personalfile_deleted'] = '文件删除成功。';
+$string['personalfile_existing'] = '已上传的个性化文件';
+$string['personalfile_nofiles'] = '还没有上传任何个性化文件。';
+$string['personalfile_manage'] = '管理个性化文件';
+$string['personalfile_yourfile'] = '您的个性化文件';
+$string['personalfile_notfound'] = '未找到您的个性化文件。';
+$string['filename'] = '文件名';
+$string['timeuploaded'] = '上传时间';
